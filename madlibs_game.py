cd149e93791eb262b619d9c385e0af2247b21bc8
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-05 13:04:09 +0300

    Update madlibs_game.py

--- madlibs_game.py
+++ madlibs_game.py
@@ -3,7 +3,7 @@ verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 famous_tech_person = input("Famous tech person: ")
 
-madlib = f" Searching for a job is so {adjective}! \ 
+madlib = f"Searching for a job is so {adjective}! \ 
 I like to {verb1}. Stay positive and {verb2} like you are {famous_tech_person}!"
 
 print(madlib)
